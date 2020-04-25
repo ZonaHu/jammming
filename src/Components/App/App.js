24be307fa61a51588ce146733474557277694182
ZonaHu <hzm5622@126.com>
2020-04-25 17:59:14 -0400

    Playlist Added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
   render(){
@@ -10,8 +12,8 @@ class App extends React.Component {
           <div className="App">
             <SearchBar />
             <div className="App-playlist">
-              {/*<SearchResults />*/}
-              {/*<Playlist />*/}
+              <SearchResults />
+              <Playlist />
             </div>
           </div>
         </div>
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './SearchResults.css'
+
+class SearchResults extends  React.Component {
+    render(){
+        return(
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/*<Tracklist />*/}
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
