c3d1e685b8d2408fe7875617f6c2afff7fee4200
ZonaHu <hzm5622@126.com>
2020-04-26 01:28:47 -0400

    More

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import './SearchResults.css'
 
+import TrackList from "../TrackList/TrackList";
+
 class SearchResults extends  React.Component {
     render(){
         return(
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/*<Tracklist />*/}
+                <TrackList tracks={this.props.searchResults} />
             </div>
         )
     }
