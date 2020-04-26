f3b977a310dd958e9653e014712734bdae62e12e
ZonaHu <hzm5622@126.com>
2020-04-26 16:47:54 -0400

    More added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
                 id: 6}]
         };
         this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
     }
 
     addTrack(track){
@@ -30,6 +31,15 @@ class App extends React.Component {
         tracks.push(track);
         this.setState({playlistTracks: tracks});
     }
+
+    removeTrack(track){
+        let tracks = this.state.playlistTracks;
+        tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+        this.setState({playlistTranks: tracks});
+
+    }
+
   render(){
     return (
         <div>
@@ -40,7 +50,8 @@ class App extends React.Component {
               <SearchResults searchResults={this.state.searchResults}
                             onAdd={this.addTrack}/>
               <Playlist playlistName={this.state.playlistName}
-                        playlistTracks = {this.state.playlistTracks}/>
+                        playlistTracks = {this.state.playlistTracks}
+                        onRemove={this.removeTrack} />
             </div>
           </div>
         </div>
