b1fe2233507e2e35308a790c65d73f025323461f
ZonaHu <hzm5622@126.com>
2020-05-05 23:27:16 -0400

    Search added

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,6 +3,15 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props) ;
+
+        this.search = this.search.bind(this);
+    }
+
+    search(){
+        this.props.onSearch(this.state.term);
+    }
     render(){
         return (
             <div className="SearchBar">
