0b2a8e5cb95cef75d8f6e7818ac6ddbfb69a5e8b
ZonaHu <hzm5622@126.com>
2020-06-10 15:28:46 -0400

    Search Bar updated

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,6 +6,10 @@ class SearchBar extends React.Component{
     constructor(props){
         super(props) ;
 
+        this.state = {
+            term:''
+        }
+        
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
@@ -21,7 +25,7 @@ class SearchBar extends React.Component{
     render(){
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist"/>
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
