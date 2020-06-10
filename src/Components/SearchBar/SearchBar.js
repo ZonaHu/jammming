6e2cab7c0f55a200922c10fdd4c9310ce856fe7c
ZonaHu <hzm5622@126.com>
2020-06-10 15:24:03 -0400

    Search Bar updated

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,11 +7,17 @@ class SearchBar extends React.Component{
         super(props) ;
 
         this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
     }
 
     search(){
         this.props.onSearch(this.state.term);
     }
+
+    handleTermChange(event){
+        this.setState({term:event.target.value});
+    }
+    
     render(){
         return (
             <div className="SearchBar">
