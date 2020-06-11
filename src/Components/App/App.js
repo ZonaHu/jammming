14da5478e6427f9f7a2af9ee232a50f326dcd074
ZonaHu <hzm5622@126.com>
2020-06-10 21:12:19 -0400

    Updated the state of searchResults.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,8 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
+import Spotify from "../../util/Spotify";
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -52,7 +54,9 @@ class App extends React.Component {
     }
 
     search(term){
-        console.log(term);
+       Spotify.search(term).then(searchResults => {
+           this.setState({searchResults: searchResults})
+       })
     }
 
   render(){
